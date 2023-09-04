da7a2851333bcff0e50ad70938e7ab6b96f8bdc1
Anton Tayanovskyy <anton.tayanovskyy@gmail.com>
2023-09-03 21:53:32 -0400

    More tests for j diffs

---
+++ j_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDiffJ(t *testing.T) {
+	x := mustJ(map[string]interface{}{
+		"a": 1,
+		"b": "foo",
+		"c": []interface{}{1, 2, 3, 4},
+	})
+	y := mustJ(map[string]interface{}{
+		"a": 2,
+		"b": "foo",
+		"c": []interface{}{0, 1, 2, 5},
+	})
+
+	dxy, xneqy := x.diff(y)
+
+	assert.True(t, xneqy)
+	assert.Equal(t, map[string]interface{}{
+		"a": map[string]interface{}{
+			"+": float64(2),
+			"-": float64(1),
+		},
+		"c": []interface{}{
+			map[string]interface{}{"+": float64(0)},
+			float64(1),
+			float64(2),
+			map[string]interface{}{"-": float64(3)},
+			map[string]interface{}{"-": float64(4)},
+			map[string]interface{}{"+": float64(5)},
+		},
+	}, dxy.v)
+}
